8e347396503dd5687580c15aca24436793c6f10d
priyakumari-bu <56745980+priyakumari-bu@users.noreply.github.com>
2020-05-06 15:55:35 -0400

    Update CompileMe.java

--- CompileMe.java
+++ CompileMe.java
@@ -1,7 +1,8 @@
 import java.util.Date;
 
 // class for compilation purposes
-
+// contains the main method
+// creates an instance of the bank and persistancehandler for persistance
 public class CompileMe {
     public static void main(String[] args) {
         Bank bank = Bank.getInstance();
