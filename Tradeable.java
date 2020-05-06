38b0b4850b079de607d775d76ca0e4770d408324
priyakumari-bu <56745980+priyakumari-bu@users.noreply.github.com>
2020-05-06 16:35:23 -0400

    Update Tradeable.java

--- Tradeable.java
+++ Tradeable.java
@@ -1,3 +1,5 @@
+// interface implemented by Stock that represents a stock's ability to be bought and sold (traded) 
+// created so that future derivatives like bonds or options can also be traded in the market 
 interface Tradeable {
 
     public int getVolume();
