a39be62a14e56f52d53ccf29a56be781a4bfc147
priyakumari-bu <56745980+priyakumari-bu@users.noreply.github.com>
2020-05-06 15:52:52 -0400

    Update BankComponent.java

--- BankComponent.java
+++ BankComponent.java
@@ -1,6 +1,6 @@
-
+// Interface that all of the bank components (users, accounts, etc.) implement 
 public interface BankComponent {
 
-    ID getID();
+    ID getID(); // all bank components must have a getID method as they all have a unique ID 
 
 }
